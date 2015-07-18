8e37b5a7c0ab3d9ef77ae83210827154abb2db6e
Jim Allaire <james.allaire@gmail.com>
2015-07-18 12:05:06 -0400

    themes
    title changes

--- dorothyday/_books/make-theme-books.sh
+++ dorothyday/_books/make-theme-books.sh
@@ -1,12 +1,13 @@
 
 #!/bin/bash
 
-./make-theme-book.sh "On Work" 154 227 229 431 452
-./make-theme-book.sh "On War and Peace" 215 306 360 868 390 250
-./make-theme-book.sh "On Peter Maurin" 256 151 148 914 152 155 468 176 495
-./make-theme-book.sh "On Hospitality" 435 308 358 416 189
-./make-theme-book.sh "On Aims and Purposes" 12 266 182 919 155 456 158 170 177 529
-./make-theme-book.sh "On Poverty" 633 560 486 150 468 230 641 647 812 838 
+#./make-theme-book.sh "On Work" 154 227 229 431 452
+#./make-theme-book.sh "On War and Peace" 215 306 360 868 390 250
+#./make-theme-book.sh "On Peter Maurin" 256 151 148 914 152 155 468 176 495
+#./make-theme-book.sh "On Hospitality" 435 308 358 416 189
+#./make-theme-book.sh "On Aims and Purposes" 12 266 182 919 155 456 158 170 177 529
+#./make-theme-book.sh "On Poverty" 633 560 486 150 468 230 641 647 812 838
+./make-theme-book.sh "Collection One: 1933-1935" 12 934 266 267 884 269 268 270 274 272 271 197 275 936 935 276 198 277 937 311 13 938 279 278 280 281 282 283 199 925 284 214 15 286 287 288 290 289 939 291 292 293 294 16 295
 
 
 
